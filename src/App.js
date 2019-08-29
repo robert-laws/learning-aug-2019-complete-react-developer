bba33ced6fe850b82170cfdb272da4d020dd319d
Robert Laws <roblaws@me.com>
2019-08-29 14:34:49 +0300

    basic project setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component} from 'react';
 
+import HomePage from './components/homepage.component';
+
 import './App.scss';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>New App</h1>
+        <HomePage />
       </div>
     );
   }
