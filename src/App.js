2a0f9f3e5407d303340074070d94bd5b6bd44f16
Robert Laws <roblaws@me.com>
2019-08-29 14:15:26 +0300

    app refactor to beginning point

--- src/App.js
+++ src/App.js
@@ -1,41 +1,12 @@
 import React, { Component} from 'react';
-import { CardList } from './components/card-list/card-list.component';
-import { SearchBox } from './components/search-box/search-box.component';
 
 import './App.scss';
 
 class App extends Component {
-  componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
-      .then(response => response.json())
-      .then(users => this.setState({ characters: users}))
-  }
-
-  constructor() {
-    super();
-
-    this.state = {
-      characters: [],
-      searchField: ''
-    }
-  }
-
-  handleChange = event => {
-    this.setState({
-      searchField: event.target.value
-    })
-  }
-
   render() {
-    const { characters, searchField } = this.state;
-    const filteredCharacters = characters.filter(character => character.name.toLowerCase().includes(searchField.toLowerCase()))
-    
-
     return (
       <div className="App">
-        <h1>Characters Rolodex</h1>
-        <SearchBox placeholder='search characters' handleChange={this.handleChange} value={this.state.searchField} />
-        <CardList characters={filteredCharacters} />
+        <h1>New App</h1>
       </div>
     );
   }
